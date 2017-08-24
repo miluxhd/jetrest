83e6bcdabdea85653b9ff8e6c8d473223f42278c
milad zangeneh <miladx7268@gmail.com>
2017-08-24 19:21:20 +0430

    تغییر آدرس رست

--- src/main/java/ir/milux/Resource.java
+++ src/main/java/ir/milux/Resource.java
@@ -9,7 +9,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/search")
+@Path("/rest")
 public class Resource extends  ResourceConfig{
 
     @POST
